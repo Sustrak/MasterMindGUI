be7178ec5038b939898d7f2aea48578e998ea3a3
Oriol Mari <oriolmarimarques@gmail.com>
2018-01-05 20:26:46 +0100

    miniGUIbugs

---
+++ src/main/java/view/LoadGameViewController.java
@@ -0,0 +1,71 @@
+package view;
+
+import game.DiffEnum;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import layers.DomainCtrl;
+import rr.RREntry;
+
+import java.net.MalformedURLException;
+import java.util.Date;
+
+
+public class LoadGameViewController {
+
+    private DomainCtrl domainCtrl;
+
+    public VBox mainVBox;
+    public Label difficultyLabel;
+    public TableView<RREntry> rankingTable;
+    public TableColumn<RREntry, Date> dateColumn;
+    public TableColumn<RREntry, String> nickNameColumn;
+    public TableColumn<RREntry, Double> scoreColumn;
+
+    public void setDomainCtrl(DomainCtrl domainCtrl) {
+        this.domainCtrl = domainCtrl;
+        buildRankingsGridPane();
+    }
+
+    private void buildRankingsGridPane() {
+        
+        RREntry rankingList[] = new RREntry[0];
+
+        switch (ViewController.askCodeBreakerDifficulty()) {
+            case EASY:
+                rankingList = domainCtrl.getRankingRREntryes(DiffEnum.EASY);
+                difficultyLabel.setText("Ranking dificultad Fácil");
+                break;
+            case ORIGINAL:
+                rankingList = domainCtrl.getRankingRREntryes(DiffEnum.ORIGINAL);
+                difficultyLabel.setText("Ranking dificultad Original");
+                break;
+            case HARD:
+                rankingList = domainCtrl.getRankingRREntryes(DiffEnum.HARD);
+                difficultyLabel.setText("Ranking dificultad Difícil");
+                break;
+        }
+
+        dateColumn.setCellValueFactory(
+                new PropertyValueFactory<RREntry, Date>("date"));
+
+        nickNameColumn.setCellValueFactory(
+                new PropertyValueFactory<RREntry, String>("username"));
+
+        scoreColumn.setCellValueFactory(
+                new PropertyValueFactory<RREntry, Double>("score"));
+
+        rankingTable.getItems().setAll(rankingList);
+    }
+
+    public void exitButtonAction(ActionEvent actionEvent) {
+        try {
+            ViewController.mainMenuView(domainCtrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
